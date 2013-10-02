bf2b40ad2b1fd26721359c00222439ca35dc00e6
Ralf Winkler <winkler.phonetics@googlemail.com>
2013-10-02 15:39:09 +0200

    code cleaning: points ant contours are now stored 2D only II

--- m_files_utillity/get_grdLine_Translation.m
+++ m_files_utillity/get_grdLine_Translation.m
@@ -6,12 +6,12 @@ tMat = tmat_init;
 % translate, i.e. 3 mm upward
 tMat = tmat_trans(tMat, [0 0 myDist]);
 % translate to origin
-tMat = tmat_trans(tMat, -vec_trans);
+tMat = tmat_trans(tMat, -vec_trans');
 % rotate
 tMat = tmat_rot_axis(tMat, -anglDeg, 'X');
 % translate
-tMat = tmat_trans(tMat, pt_rot);
+tMat = tmat_trans(tMat, pt_rot');
 
 % transform points
-ptInnerNew = tmat_mxp(tMat, ptInner');
-ptOuterNew = tmat_mxp(tMat, ptOuter');
+ptInnerNew = tmat_mxp(tMat, ptInner);
+ptOuterNew = tmat_mxp(tMat, ptOuter);
