3cdcc261186871527c9ab08a25270879eb07e733
Lasse Bombien <lasse@herbert.local>
2013-11-18 12:16:42 -0800

    Fix object prefixing
    Some variables were incorrectly or not prefixed

--- @tongueSim/K.m
+++ @tongueSim/K.m
@@ -13,7 +13,6 @@ function K=K(TSObj, r, s, XY, lambda2, mu2, pfix)
 %                1            2
 % sachant que si 1 est fixe alors 3 est fixe aussi
 
-
 x=XY(TSObj.IX);
 y=XY(TSObj.IY);
 
@@ -32,8 +31,8 @@ elseif pfix==1
   IAfix=[9*ones(1,16),TSObj.IA(17:32),9*ones(1,16),TSObj.IA(49:64)];
   IDfix=[9*ones(1,16),TSObj.ID(17:32),9*ones(1,16),TSObj.ID(49:64)];
 elseif pfix==2
-  IAfix=[IA(1:16),9*ones(1,16),TSObj.IA(33:64)];
-  IDfix=[ID(1:16),9*ones(1,16),TSObj.ID(33:64)];
+  IAfix=[TSObj.IA(1:16),9*ones(1,16),TSObj.IA(33:64)];
+  IDfix=[TSObj.ID(1:16),9*ones(1,16),TSObj.ID(33:64)];
 elseif pfix==3
   IAfix=[TSObj.IA(1:32),9*ones(1,16),TSObj.IA(48:64)];
   IDfix=[TSObj.ID(1:32),9*ones(1,16),TSObj.ID(48:64)];
