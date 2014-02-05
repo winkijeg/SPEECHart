5cf285b4d0672d0e56fff2109ae8f2413a72132e
Ralf Winkler <winkler.phonetics@googlemail.com>
2014-02-05 21:24:53 +0100

    refactoring code and add descriptions

--- code_imaging/@SpeakerImaging/resampleMidSagittSlice.m
+++ code_imaging/@SpeakerImaging/resampleMidSagittSlice.m
@@ -1,19 +1,16 @@
 function obj = resampleMidSagittSlice(obj, targetPixelWidth, targetPixelHeight)
-% resampling means transform the picture but NOT the geometrical data
-% (xdata & ydata)
-
+% manipulate slice resolution while preserving its geometric dimension
 
 % calculate enlargement factor
 fac_enlarge_x = obj.sliceInfo.PixelDimensions(2) / targetPixelWidth;
 fac_enlarge_y =  obj.sliceInfo.PixelDimensions(3) / targetPixelHeight;
 
+tform = maketform('affine', ...
+    [fac_enlarge_x*obj.sliceInfo.PixelDimensions(2) 0 0; ...
+    0 fac_enlarge_y*obj.sliceInfo.PixelDimensions(3) 0; ...
+    0 0 1]);
 
-tform = maketform('affine', [fac_enlarge_x*obj.sliceInfo.PixelDimensions(2) 0 0;...
-        0 fac_enlarge_y*obj.sliceInfo.PixelDimensions(3) 0; 0 0 1]);
-
-    
 [obj.sliceData, ~, ~] = imtransform(obj.sliceData, tform, 'bicubic', ...
     'FillValues', 0, 'XYScale', 1);
 
 end
-
