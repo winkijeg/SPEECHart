22680a3b88f08765d0c2830a92e16b50b83f4734
Ralf Winkler <winkler.phonetics@googlemail.com>
2014-02-05 21:24:53 +0100

    refactoring code and add descriptions

--- code_imaging/@SpeakerImaging/calcGridlineOfBending.m
+++ code_imaging/@SpeakerImaging/calcGridlineOfBending.m
@@ -1,32 +1,34 @@
-function indBending = calcGridlineOfBending(innerPt, outerPt, ptCircleMidpoint, ptNPW_d)
-    % derives position of VT bending in a radial grid based on ANS-PNS plane
+function indBending = calcGridlineOfBending(innerPt, outerPt, ...
+    ptCircleMidpoint, ptNPW_d)
+% derive VT bending position based on a radial grid relative to ANS-PNS plane
 
-    percentDeviation = 5; % default = 5
+maxTolerance = 5; % default deviation allowed = 5 degrees
 
-    nGrdLines = size(innerPt, 2);
+nGrdLines = size(innerPt, 2);
 
-    % find the gridline that devide pharynx from oral cavity, ...
-    % hence, front cavity from back cavity 
-
-    k = 1;
-    isAngleGreaterFive = 1;
-    while ((isAngleGreaterFive) && (k <= nGrdLines))
-
-        nbGridLine = k;
-        ptInnerTmp = innerPt(1:2, nbGridLine);
-        ptOuterTmp = outerPt(1:2, nbGridLine);
-
-        angleRadTmp = lines_exp_angle_nd(2, ptInnerTmp', ptOuterTmp', ptCircleMidpoint', ptNPW_d');
-        angleDeg = radians_to_degrees(angleRadTmp);
-
-        if angleDeg < percentDeviation
-            isAngleGreaterFive = 0;
-            grdLineBorder = nbGridLine;
-        end  
-        k = k + 1;
+% find the gridline that devide pharynx from oral cavity, ...
+% hence, front cavity from back cavity
+k = 1;
+isAngleAboveMaxTolerance = true;
+while ((isAngleAboveMaxTolerance) && (k <= nGrdLines))
+    
+    nbGridLine = k;
+    ptInnerTmp = innerPt(1:2, nbGridLine);
+    ptOuterTmp = outerPt(1:2, nbGridLine);
+    
+    angleRadTmp = lines_exp_angle_nd(2, ptInnerTmp', ptOuterTmp', ...
+        ptCircleMidpoint', ptNPW_d');
+    angleDeg = radians_to_degrees(angleRadTmp);
+    
+    if angleDeg < maxTolerance
+        isAngleAboveMaxTolerance = false;
+        grdLineBorder = nbGridLine;
     end
+    
+    k = k + 1;
+    
+end
 
-    % assign values ---------------------------------------------------
-    indBending = grdLineBorder;
+indBending = grdLineBorder;
 
 end
