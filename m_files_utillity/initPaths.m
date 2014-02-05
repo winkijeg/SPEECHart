f2817b01ea99b7b7128e83eb387bf54b50a7b3a1
Ralf Winkler <winkler.phonetics@googlemail.com>
2014-02-05 18:51:20 +0100

    correct path name pointing to the new temporary project name (opossum)

--- m_files_utillity/initPaths.m
+++ m_files_utillity/initPaths.m
@@ -1,6 +1,6 @@
 function [root, model, mri] = initPaths(princInvest, spkName)
     
-    root = 'e:/projects/project_refactoring/code_matlab/';
+    root = 'e:/projects/project_refactoring/opossum/';
     
     model = [root 'data/db_examples/' princInvest '_' spkName '/speakerModel/'];
     mri = [root 'data/db_examples/' princInvest '_' spkName '/speakerImaging/'];
