6e1b9206529008cbf250ad04ee065ca4844cf7d7
Ralf Winkler <winkler.phonetics@googlemail.com>
2014-02-06 15:18:29 +0100

    refactor variable names and add documentation

--- code_imaging/@SpeakerImaging/calculateMinBetweenContours.m
+++ code_imaging/@SpeakerImaging/calculateMinBetweenContours.m
@@ -1,18 +1,18 @@
 function [valMin, indMin] = calculateMinBetweenContours(innerCont, outerCont)
+% calculate minimal constriction width between inner and outer VT contour
 
-    nPoints = size(innerCont, 2);
-    
-    distances = zeros(1, nPoints);
-    for k = 1:nPoints
-        
-        p1 = innerCont(1:2, k);
-        p2 = outerCont(1:2, k);
-        
-        distances(k) = points_dist_nd(2, p1', p2');
-        
-    end
-    
-    [valMin, indMin] = min(distances);
+nPoints = size(innerCont, 2);
 
+distances = nan(1, nPoints);
+for k = 1:nPoints
+    
+    pt1 = innerCont(1:2, k);
+    pt2 = outerCont(1:2, k);
+    
+    distances(k) = points_dist_nd(2, pt1', pt2');
+    
 end
 
+[valMin, indMin] = min(distances);
+
+end
