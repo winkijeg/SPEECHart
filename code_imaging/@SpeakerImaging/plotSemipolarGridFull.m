8a4e83021a1a32d39bba8ba41dc9914e5a179fe8
Ralf Winkler <winkler.phonetics@googlemail.com>
2014-02-06 17:13:42 +0100

    extract function that plots the full semipolar grid

---
+++ code_imaging/@SpeakerImaging/plotSemipolarGridFull.m
@@ -0,0 +1,25 @@
+function [] = plotSemipolarGridFull(obj, col, nbsOfGrdLines, cAxes)
+% plot semipolar grid stored in the SpeakerImaging object
+%   takes an optional third argument gridLines in order to plot a part of the
+%   grid (i.e. 3:7) or only one gridline (i.e. 10)
+
+if nargin == 2
+    nbsOfGrdLines = 1:obj.semipolarGrid.numberOfGridlines;
+end
+
+nGrdLinesToPlot = length(nbsOfGrdLines);
+
+innerXval = obj.semipolarGrid.innerPt(1, nbsOfGrdLines(1):nbsOfGrdLines(end));
+outerXval = obj.semipolarGrid.outerPt(1, nbsOfGrdLines(1):nbsOfGrdLines(end));
+
+innerYval = obj.semipolarGrid.innerPt(2, nbsOfGrdLines(1):nbsOfGrdLines(end));
+outerYval = obj.semipolarGrid.outerPt(2, nbsOfGrdLines(1):nbsOfGrdLines(end));
+
+for k = 1:nGrdLinesToPlot
+    
+    line([innerXval(k) outerXval(k)], ...
+        [innerYval(k) outerYval(k)], 'Color', col, 'Parent', cAxes)
+    
+end
+
+end
