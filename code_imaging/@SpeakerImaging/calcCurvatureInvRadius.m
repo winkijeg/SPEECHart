007937c67a35e89be03ffbb480a72e81af9c6b2b
Ralf Winkler <winkler.phonetics@googlemail.com>
2014-02-07 17:07:47 +0100

    rename static function

--- code_imaging/@SpeakerImaging/calcCurvatureInvRadius.m
+++ code_imaging/@SpeakerImaging/calcCurvatureInvRadius.m
@@ -1,4 +1,4 @@
-function [val, basicData] = determineCurvatureInvRadius(ptStart, ptMid, ptEnd)
+function [val, basicData] = calcCurvatureInvRadius(ptStart, ptMid, ptEnd)
 % determine curvature by means of invert radius of a circle crossing 3 points
 
 val = segments_curvature_2d(ptStart', ptMid', ptEnd');
