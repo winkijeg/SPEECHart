4d596ee052cf61b940258becd36d2ac23af127d7
Ralf Winkler <winkler.phonetics@googlemail.com>
2014-02-07 12:48:49 +0100

    refactor and add help text to files that reformat speaker data into model format

--- code_imaging/@SpeakerImaging/exportLandmarksToModelFormat.m
+++ code_imaging/@SpeakerImaging/exportLandmarksToModelFormat.m
@@ -1,20 +1,17 @@
 function landmarks = exportLandmarksToModelFormat(obj)
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
+% reformat anatomical landmarks of a speaker into model format
 
-    landmarks.styloidProcess = obj.landmarks.Stylo;
-    
-    % the three hyoid points will be transformed (later)
-    landmarks.condyle = [obj.landmarks.Stylo(1); obj.landmarks.Stylo(2)+8];
-    landmarks.tongInsL = obj.landmarks.TongInsL;
-    landmarks.tongInsH = obj.landmarks.TongInsH;
-    
-    from = obj.gridZoning.tongue(1);
-    to = obj.gridZoning.tongue(2);
-    tongSurfTmp = obj.filteredContours.innerPt(1:2, from:to);
-    
-    landmarks.origin = mean(tongSurfTmp, 2);
-    
+landmarks.styloidProcess = obj.landmarks.Stylo;
+
+% the three hyoid points will be transformed (later)
+landmarks.condyle = [obj.landmarks.Stylo(1); obj.landmarks.Stylo(2)+8];
+landmarks.tongInsL = obj.landmarks.TongInsL;
+landmarks.tongInsH = obj.landmarks.TongInsH;
+
+from = obj.gridZoning.tongue(1);
+to = obj.gridZoning.tongue(2);
+tongSurfTmp = obj.filteredContours.innerPt(1:2, from:to);
+
+landmarks.origin = mean(tongSurfTmp, 2);
 
 end
-
