b9606f8410a92d092263903335a16e162d63898e
Ralf Winkler <winkler.phonetics@googlemail.com>
2014-02-07 19:34:52 +0100

    add help text

--- m_files_utillity/findMinDistanceBetweenTwoLines.m
+++ m_files_utillity/findMinDistanceBetweenTwoLines.m
@@ -1,5 +1,9 @@
 function [valMin, indMin] = findMinDistanceBetweenTwoLines(line1, Line2)
 % calculate minimal distance between two lines of equal length
+%   input 1: 2d line of n points, dim 2 x n
+%   input 2: 2d line of n points, dim 2 x n
+%
+%   the two lines should have the same number of points
 
 nPoints = size(line1, 2);
 
