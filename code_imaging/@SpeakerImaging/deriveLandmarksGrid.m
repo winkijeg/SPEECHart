807720eef177aa51b30b4098808182c8f15b6c3a
Ralf Winkler <winkler.phonetics@googlemail.com>
2014-02-07 17:21:50 +0100

    refactor and add help text

--- code_imaging/@SpeakerImaging/deriveLandmarksGrid.m
+++ code_imaging/@SpeakerImaging/deriveLandmarksGrid.m
@@ -1,17 +1,15 @@
 function landmarksDerived = deriveLandmarksGrid(obj)
+% calculate anatomical landmarks neccessary for semipolar grid generation
 
-    p_AlvRidge = obj.landmarks.AlvRidge;
-    p_Palate = obj.landmarks.Palate;
-    p_PharH_d = obj.landmarksDerivedMorpho.PharH_d;
-    
-   
-    % calculate midpointCircle, the center of a circle intersecting the 
-    % landmarks p_AlvRidge, p_Palate, p_PharH_d
-    pointsTmp = [p_AlvRidge p_Palate p_PharH_d];
-    [~, midPoint2D(:, 1)] = triangle_circumcircle_2d(pointsTmp);
-    
-    % assign values -------------------------------------------
-    landmarksDerived.circleMidpoint = midPoint2D;
-  
+p_AlvRidge = obj.landmarks.AlvRidge;
+p_Palate = obj.landmarks.Palate;
+p_PharH_d = obj.landmarksDerivedMorpho.PharH_d;
+
+% calculate midpointCircle, the center of a circle intersecting the
+% landmarks p_AlvRidge, p_Palate, p_PharH_d
+pointsTmp = [p_AlvRidge p_Palate p_PharH_d];
+[~, midPoint2D(:, 1)] = triangle_circumcircle_2d(pointsTmp);
+
+landmarksDerived.circleMidpoint = midPoint2D;
+
 end
-
