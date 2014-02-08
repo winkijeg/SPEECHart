7766c5191222841606830c730d318bff54719263
Ralf Winkler <winkler.phonetics@googlemail.com>
2014-02-08 13:16:37 +0100

    rename function

--- code_imaging/@SpeakerImaging/plotSemipolarGridPart.m
+++ code_imaging/@SpeakerImaging/plotSemipolarGridPart.m
@@ -1,4 +1,4 @@
-function [] = plotSemipolarGridPart(obj, col, nbsOfGrdLines, cAxes)
+function [] = plotSemipolarGridPart(obj, col, grdLines, cAxes)
 % plot partially semipolar grid specified by the third argument
 %   i.e. 3:7 or only one gridline (i.e. 10)
 
@@ -7,13 +7,13 @@ if nargin == 3
     cAxes = initPlotFigure(obj, false);
 end
 
-nGrdLinesToPlot = length(nbsOfGrdLines);
+nGrdLinesToPlot = length(grdLines);
 
-innerXval = obj.semipolarGrid.innerPt(1, nbsOfGrdLines(1):nbsOfGrdLines(end));
-outerXval = obj.semipolarGrid.outerPt(1, nbsOfGrdLines(1):nbsOfGrdLines(end));
+innerXval = obj.semipolarGrid.innerPt(1, grdLines(1):grdLines(end));
+outerXval = obj.semipolarGrid.outerPt(1, grdLines(1):grdLines(end));
 
-innerYval = obj.semipolarGrid.innerPt(2, nbsOfGrdLines(1):nbsOfGrdLines(end));
-outerYval = obj.semipolarGrid.outerPt(2, nbsOfGrdLines(1):nbsOfGrdLines(end));
+innerYval = obj.semipolarGrid.innerPt(2, grdLines(1):grdLines(end));
+outerYval = obj.semipolarGrid.outerPt(2, grdLines(1):grdLines(end));
 
 for k = 1:nGrdLinesToPlot
     
