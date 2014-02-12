85f5f70addad73dea86e52ff553090deecbc744e
Ralf Winkler <winkler.phonetics@googlemail.com>
2014-02-12 16:52:26 +0100

    re-organize model matching

--- code_modelling/@ModelProducer/ModelProducer.m
+++ code_modelling/@ModelProducer/ModelProducer.m
@@ -5,27 +5,64 @@ classdef ModelProducer
         
         modelGeneric
         
-        tformImg
+        landmarks
+        contours
+        gridZoning
+
         tMatGeom
+        tformImg
+
+        contoursTransformed
+        landmarksTransformed
+        anatomicalStructures
+        
         
     end
     
     methods
         
-        function obj = ModelProducer()
+        function obj = ModelProducer(dataSpkMRI, gridZoning)
             
             matModelGeneric = load('ypm_model.mat');
             obj.modelGeneric = SpeakerModel(matModelGeneric);
             
+            obj.landmarks.styloidProcess = dataSpkMRI.styloidProcess;
+            obj.landmarks.condyle = dataSpkMRI.condyle;
+            obj.landmarks.tongInsL = dataSpkMRI.tongInsL;
+            obj.landmarks.tongInsH = dataSpkMRI.tongInsH;
+            obj.landmarks.ANS = dataSpkMRI.ANS;
+            obj.landmarks.PNS = dataSpkMRI.PNS;
+            obj.landmarks.origin = dataSpkMRI.origin;
+            
+            obj.contours.innerPt = dataSpkMRI.innerPt;
+            obj.contours.outerPt = dataSpkMRI.outerPt;
+            
+            obj.gridZoning = gridZoning;
+        
+            % calculate initial rotation of mri data
+            [obj.tMatGeom, obj.tformImg] = calcTransformationImgToModel(obj);
+            
+            % rotate mri data of specific speaker
+            [lmTrans, contsTrans] = transformSpeakerData(obj);
+            obj.landmarksTransformed = lmTrans;
+            obj.contoursTransformed = contsTrans;
+            
+            % extract anatomical structures
+            obj.anatomicalStructures = convertContoursIntoStructures(obj);
+            
+            
         end
-        
-        structsAnatomical = convertContoursIntoStructures(obj, strucTransformed, gridZoning)
-        
-        [tMatGeom, tformImg] = calcTransformationImgToModel(obj, matRawModelTarget);
-        strucTransformed = transformSpeakerData(obj, strucOrig, tMat);
 
-        struc = matching2D_ForRefactoring(obj, matRawModelTarget);
+        dataModel = matching2D_ForRefactoring( obj );
 
+    end
+    
+    methods (Access = private)
+        
+        [tMatGeom, tformImg] = calcTransformationImgToModel(obj);
+        [landmarksTrans, contoursTrans] = transformSpeakerData(obj);
+        anatomicalStructures = convertContoursIntoStructures(obj);
+        
     end
         
 end
