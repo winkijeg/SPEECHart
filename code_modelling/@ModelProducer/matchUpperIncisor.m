c96091b3541998791c96b4b4c3285dae48fe14f6
Ralf Winkler <winkler.phonetics@googlemail.com>
2014-02-18 17:30:17 +0100

    split transformation into separate scale and translation operations

--- code_modelling/@ModelProducer/matchUpperIncisor.m
+++ code_modelling/@ModelProducer/matchUpperIncisor.m
@@ -5,25 +5,34 @@ function [upperIncisorPalate, ptAttachLip] = matchUpperIncisor(obj, palate, dist
 scaleFactor = min(1, distRatioTeethInsertionPoints);
 
 palateLeftToRightOrder = fliplr(palate);
-ptIncisorPalate = palateLeftToRightOrder(1:2, 1);
+ptAttachIncisor = palateLeftToRightOrder(1:2, 1);
+
 
 mat = load('upperIncisorStandard.mat');
+indexAttachLip = mat.indexAttachLip;
+% todo : mat.indexLastIncisorPoint is not necessary after refactoring (scale and
+% trans)
+upperIncisorStandard = mat.upperIncisorStandard;
+nPointsIncisor = size(upperIncisorStandard, 2);
+upperIncisorStandard3D = [zeros(1, nPointsIncisor); upperIncisorStandard];
 
-nPointsIncisor = mat.indexLastIncisorPoint;
-upperIncisorStandard = mat.upperIncisorStandard(1:2, 1:nPointsIncisor);
+tMat = tmat_init();
 
-% adapt incisor size
-incisorNew = nan(2, nPointsIncisor);
-for nbPtIncisor = 1:nPointsIncisor
-    
-    incisorNew(1:2, nbPtIncisor) = ptIncisorPalate + ...
-        scaleFactor * upperIncisorStandard(1:2, nbPtIncisor);
-    
-end
+% scale
+scaleVec = [0 scaleFactor scaleFactor];
+t1 = tmat_scale(tMat, scaleVec);
+incisor3DTemp = tmat_mxp2(t1, nPointsIncisor, upperIncisorStandard3D);
+% determine neccesary shift to attach upper incisor
+translateVec = [0 ptAttachIncisor'] - incisor3DTemp(1:3, nPointsIncisor)';
+
+% translate 
+t2 = tmat_trans(t1, translateVec);
+
+incisorTrans = tmat_mxp2(t2, nPointsIncisor, upperIncisorStandard3D);
+incisorNew(1:2, :) = incisorTrans(2:3, :);
 
 upperIncisorPalate = [incisorNew palateLeftToRightOrder];
-ptAttachLip = upperIncisorPalate(1:2, mat.indexAttachLip);
-
+ptAttachLip = upperIncisorPalate(1:2, indexAttachLip);
 
 end
 
