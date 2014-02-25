0399368c205d604289378c153a85ee11b7235b30
Ralf Winkler <winkler.phonetics@googlemail.com>
2014-02-25 11:38:07 +0100

    add help text

--- code_modelling/@PositionFrame/getPositionOfTongSurface.m
+++ code_modelling/@PositionFrame/getPositionOfTongSurface.m
@@ -1,6 +1,5 @@
 function pts = getPositionOfTongSurface(obj)
-%UNTITLED4 Summary of this function goes here
-%   Detailed explanation goes here
+% extract tongue surface
 
     indicesSurfaceNodes = [13 26 39 52 65 78 91 104 117 130 143 ...
         156 169 182 195 208 221];
