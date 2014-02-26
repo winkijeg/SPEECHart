9ae5e6ebaefd756c06d3a50d9952eb3d54108955
Ralf Winkler <winkler.phonetics@googlemail.com>
2014-02-26 11:10:23 +0100

    minor refactoring

--- code_modelling/@SpeakerModel/plotTongueMesh.m
+++ code_modelling/@SpeakerModel/plotTongueMesh.m
@@ -2,7 +2,7 @@ function [] = plotTongueMesh( obj, col )
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
 
-    drawMesh(obj.tongGrid, col)
+drawMesh(obj.tongGrid, col)
 
 %     plot(obj.tongGrid.x , obj.tongGrid.y, 'Color', col, 'LineWidth', 1)
 %     plot(obj.tongGrid.x' ,obj.tongGrid.y', 'Color', col, 'LineWidth', 1)
