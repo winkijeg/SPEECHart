2a13dcdc61751c5cd88a6114b814cf6cbc3eb186
Lasse Bombien <lasse@phonetik.uni-muenchen.de>
2014-03-05 15:21:01 -0800

    possibly unsafe changes
    I’m moving to a new computer

---
+++ FEM/@tongueSim/saveVars.m
@@ -0,0 +1,16 @@
+function saveVars(fileName, varargin)
+	%SAVEVARS save variables with given names to file 
+	nVar = length(varargin);
+	if (mod(nVar), 2) ~= 0
+		error('Bad input arguments'); 
+	end
+
+	names = varargin(1:2:nVar);
+	values = varargin(2:2:nVar);
+
+	for i = 1:length(names)
+		assignin('caller', names{i}, values{i});
+	end
+
+	command = ['save' fileName cellstr(names)];
+end
