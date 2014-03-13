e8acd092ba06ad0e339bd0e7cf85b1e9c242fe23
Ralf Winkler <winkler.phonetics@googlemail.com>
2014-03-13 18:02:59 +0100

    add example (delta lambda) for testing purpose

--- FEM/test_simul_tongue_adapt_jaw.m
+++ FEM/test_simul_tongue_adapt_jaw.m
@@ -3,20 +3,62 @@
 
 % written 09/2013 (Ralf)
 
-
-
-
 clear *
-op = addpath(genpath('./model_adapt_jaw'))
+op = addpath(genpath('./model_adapt_jaw'));
 
-spkStr = 'pp_FF1';
-spkStr = 'cs';
-% spkStr = 'av';
+%spkStr = 'pp_FF1';
+%spkStr = 'cs';
+spkStr = 'av';
+
+t_trans = 0.05;
+t_hold = 0.100;
+
+% % original inputs; order is GGP GGA HYO STY VER SL IL
+% seq = 'rya';
+% jaw_rot = [-3 8]:
+% lip_prot = [3 -5];
+% ll_rot = [-4 6];
+% hyoid_mov = [5 -3];
+% deltaLambda_av = [-20  20  20 -15 0 200 0; 
+%                    10 -10 -7   10 0 200 0];
+
+% % example 1 - /a/
+% seq = 'ra';
+% jaw_rot = 8;
+% lip_prot = -5;
+% ll_rot = 6;
+% hyoid_mov = -3;
+% deltaLambda_av = [10 -10 -7 10 0 0 0];
+% 
+% % example 2 - /i/
+% seq = 'ri';
+% jaw_rot = -1;
+% lip_prot = -15;
+% ll_rot = 6;
+% hyoid_mov = -13;
+% deltaLambda_av = [-10 10 7 -10 0 0 0];
+
+% example 3 - /u/
+seq = 'ru';
+jaw_rot = -1;
+lip_prot = 8;
+ll_rot = 6;
+hyoid_mov = -3;
+deltaLambda_av = [20 20 7 -20 0 0 0];
+
+
 
 path_model = ['spec_' spkStr '/'];
 
-simul_tongue_adapt_jaw(path_model, spkStr, 'rya', 'rya', [-20 10], [20 -10], ...
-    [20 -7], [-15 10], [0 0], [200 200], [0 0], [0.05 0.05], [0.100 0.100],...
-    [-3 8], [3 -5], [-4 6], [5 -3], 1);
+simul_tongue_adapt_jaw(path_model, spkStr, seq, seq, ...
+    deltaLambda_av(:, 1)', ...
+    deltaLambda_av(:, 2)', ...
+    deltaLambda_av(:, 3)', ...
+    deltaLambda_av(:, 4)', ...
+    deltaLambda_av(:, 5)', ...
+    deltaLambda_av(:, 6)', ...
+    deltaLambda_av(:, 7)', ...
+    t_trans, t_hold,...
+    jaw_rot, lip_prot, ll_rot, hyoid_mov, 1);
 
 path(op);
