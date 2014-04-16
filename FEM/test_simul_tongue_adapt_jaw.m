081faee599df46d7a314bbd720c98d1bcaf3eced
Lasse Bombien <quabolasse@googlemail.com>
2014-04-15 20:50:04 -0700

    Change Modell
    use a different speaker/sequence

--- FEM/test_simul_tongue_adapt_jaw.m
+++ FEM/test_simul_tongue_adapt_jaw.m
@@ -7,20 +7,20 @@ clear *
 op = addpath(genpath('./model_adapt_jaw'));
 
 %spkStr = 'pp_FF1';
-%spkStr = 'cs';
-spkStr = 'av';
+spkStr = 'cs';
+%spkStr = 'av';
 
 t_trans = 0.05;
 t_hold = 0.100;
 
 % % original inputs; order is GGP GGA HYO STY VER SL IL
-% seq = 'rya';
-% jaw_rot = [-3 8]:
-% lip_prot = [3 -5];
-% ll_rot = [-4 6];
-% hyoid_mov = [5 -3];
-% deltaLambda_av = [-20  20  20 -15 0 200 0; 
-%                    10 -10 -7   10 0 200 0];
+seq = 'rya';
+jaw_rot = [-3 8];
+lip_prot = [3 -5];
+ll_rot = [-4 6];
+hyoid_mov = [5 -3];
+deltaLambda_av = [-20  20  20 -15 0 200 0; 
+                    10 -10 -7   10 0 200 0];
 
 % % example 1 - /a/
 % seq = 'ra';
@@ -39,12 +39,12 @@ t_hold = 0.100;
 % deltaLambda_av = [-10 10 7 -10 0 0 0];
 
 % example 3 - /u/
-seq = 'ru';
-jaw_rot = -1;
-lip_prot = 8;
-ll_rot = 6;
-hyoid_mov = -3;
-deltaLambda_av = [20 20 7 -20 0 0 0];
+% seq = 'ru';
+% jaw_rot = -1;
+% lip_prot = 8;
+% ll_rot = 6;
+% hyoid_mov = -3;
+% deltaLambda_av = [20 20 7 -20 0 0 0];
 
 
 
--- FEM/test_tongueSim.m
+++ FEM/test_tongueSim.m
@@ -9,7 +9,7 @@ close all
 
 spkStr = 'pp_FF1';
 spkStr = 'cs';
-spkStr = 'av';
+%spkStr = 'av';
 
 t_trans = 0.05;
 t_hold = 0.100;
@@ -18,13 +18,13 @@ t_hold = 0.100;
 op=addpath(genpath('./elast'), genpath('./strjoin'));
 
 % % original inputs; order is GGP GGA HYO STY VER SL IL
-% seq = 'rya';
-% jaw_rot = [-3 8]:
-% lip_prot = [3 -5];
-% ll_rot = [-4 6];
-% hyoid_mov = [5 -3];
-% deltaLambda_av = [-20  20  20 -15 0 200 0; 
-%                    10 -10 -7   10 0 200 0];
+seq = 'rya';
+jaw_rot = [-3 8];
+lip_prot = [3 -5];
+ll_rot = [-4 6];
+hyoid_mov = [5 -3];
+deltaLambda_av = [-20  20  20 -15 0 200 0; 
+                   10 -10 -7   10 0 200 0];
 
 % % example 1 - /a/
 % seq = 'ra';
@@ -43,12 +43,12 @@ op=addpath(genpath('./elast'), genpath('./strjoin'));
 % deltaLambda_av = [-10 10 7 -10 0 0 0];
 
 % example 3 - /u/
-seq = 'ru';
-jaw_rot = -1;
-lip_prot = 8;
-ll_rot = 6;
-hyoid_mov = -3;
-deltaLambda_av = [20 20 7 -20 0 0 0];
+% seq = 'ru';
+% jaw_rot = -1;
+% lip_prot = 8;
+% ll_rot = 6;
+% hyoid_mov = -3;
+% deltaLambda_av = [20 20 7 -20 0 0 0];
 
 
 
