d982307ab83748e626c01a573f6f7dd69a44a298
r_winkler <winkler.phonetics@googlemail.com>
2015-01-06 14:50:02 +0100

    correct path letter

--- m_files_utillity/initPaths.m
+++ m_files_utillity/initPaths.m
@@ -1,6 +1,6 @@
 function [root, model, mri] = initPaths(princInvest, spkName)
     
-    root = 'e:/projects/project_refactoring/possum/';
+    root = 'd:/projects/project_refactoring/possum/';
     
     model = [root 'data/db_examples/' princInvest '_' spkName '/speakerModel/'];
     mri = [root 'data/db_examples/' princInvest '_' spkName '/speakerImaging/'];
