0bd90148a49b7ff446fc68d8774ab7bf4f04b790
r_winkler <winkler.phonetics@googlemail.com>
2015-06-12 16:34:42 +0200

    update path names

--- m_files_utillity/initPaths.m
+++ m_files_utillity/initPaths.m
@@ -1,6 +1,6 @@
 function [root, model, mri] = initPaths(princInvest, spkName)
     
-    root = 'd:/projects/project_refactoring/possum/';
+    root = 'd:/projects/project_POSSUM/possum/';
     
     model = [root 'data/db_examples/' princInvest '_' spkName '/speakerModel/'];
     mri = [root 'data/db_examples/' princInvest '_' spkName '/speakerImaging/'];
