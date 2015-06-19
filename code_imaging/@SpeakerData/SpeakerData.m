26f0d94f476bf3396bced6258c10f3430ec3f571
r_winkler <winkler.phonetics@googlemail.com>
2015-06-19 13:51:48 +0200

    new class to hold the data necessary for model building

---
+++ code_imaging/@SpeakerData/SpeakerData.m
@@ -0,0 +1,26 @@
+classdef SpeakerData
+    % store landmarks / contours determined from mid-sagittal MRI image
+    %   two contours and two kind of anatomical landmarks are necessary for
+    %   the design of speaker-specific vocal tract models.
+    
+    properties
+        speakerName = '';
+        
+        
+        
+        
+        
+    end
+    
+    methods
+        
+%         function obj = SpeakerData(struc)
+%             % object needs a structure as input arg to create a valid object
+%             
+%             obj.speakerName = 
+%             
+%         end
+    end
+    
+end
+
