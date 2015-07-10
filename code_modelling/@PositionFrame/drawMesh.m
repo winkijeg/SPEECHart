5976b68e705a0d7d3f58c4bf8feb440b85fdd539
r_winkler <winkler.phonetics@googlemail.com>
2015-07-10 20:05:40 +0200

    increase speed / remove dependency to class PositionNode

--- code_modelling/@PositionFrame/drawMesh.m
+++ code_modelling/@PositionFrame/drawMesh.m
@@ -1,13 +1,6 @@
-function [] = drawMesh(obj, col)
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
-
-    for k = 1:obj.nNodes
-        
-        plot(obj.positionNodes(k).positionX, ...
-        obj.positionNodes(k).positionY, [col '.'])
-        hold on
-       
-    end       
+function h = drawMesh(obj, col)
+    %draw complete tongue mesh 
+    
+    h = plot(obj.xValNodes, obj.yValNodes, [col '.']);
 
 end
