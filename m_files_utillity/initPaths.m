422e212c549d7d08eb323b550a0ef237fdc13d57
r_winkler <winkler.phonetics@googlemail.com>
2015-07-17 18:14:01 +0200

    remove function

--- m_files_utillity/initPaths.m
+++
@@ -1,8 +0,0 @@
-function [root, model, mri] = initPaths(princInvest, spkName)
-    
-    root = 'd:/projects/project_POSSUM/possum/';
-    
-    model = [root 'data/db_examples/' princInvest '_' spkName '/speakerModel/'];
-    mri = [root 'data/db_examples/' princInvest '_' spkName '/speakerImaging/'];
-
-end
