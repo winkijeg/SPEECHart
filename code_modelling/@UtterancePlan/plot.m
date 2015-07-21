18c63da3890068aec2696c4173bf98dc3978b473
r_winkler <winkler.phonetics@googlemail.com>
2015-07-21 10:58:23 +0200

    tune plot method

--- code_modelling/@UtterancePlan/plot.m
+++ code_modelling/@UtterancePlan/plot.m
@@ -1,43 +1,61 @@
 function [] = plot(obj, featureString, plotStr)
 %plot utterance plan specification
 
+    yValMax = 20;
+    yValMin = -20;
+    labWinHight = 3;
+
     nTargets = size(obj.target, 2);
     
-    yValsTmp = obj.(featureString);
+    featureVals = obj.(featureString);
     
+    figure
+    hold on
     
+    preceedingVal = 0;
     % loop through targets
     for nbTarget = 1:nTargets
         
-        labTmp = obj.target(nbTarget)
-        durTmp(nbTarget) = (obj.durTransition(nbTarget) + ...
-            obj.durHold(nbTarget)) * 1000
-        durSum = sum(durTmp(1:nbTarget-1))
-            
-        % first segment ist special case
-        if nbTarget == 1
-            
-            x1 = 0;
-            y1 = 0;
-            
-            x2 = durTmp(nbTarget);
-            y2 = yValsTmp(nbTarget);
-            
-            
+        labTarget{nbTarget} = obj.target(nbTarget);
+        durTarget = obj.durTransition(nbTarget) + ...
+            obj.durHold(nbTarget);
+        
+        if (nbTarget == 1)
+            t1(nbTarget) = 0;
         else
-            
-            x1 = durSum;
-            y1 = yValsTmp(nbTarget-1);
-            
-            x2 = durSum + durTmp(nbTarget);
-            y2 = yValsTmp(nbTarget);
-        
+            t1(nbTarget) = t2(nbTarget-1);
         end
-        
-        mArrow3([x1 y1 0], [x2 y2 0]);
-        %plot([x1 x2], [y1 y2]);
-        hold on;
-    end
+        t2(nbTarget) = t1(nbTarget) + durTarget;
 
+        % draw decoration 
+        rectangle('Position', [t1(nbTarget) yValMax+2 durTarget labWinHight], ...
+            'Curvature', 0.4, 'LineWidth', 1, 'LineStyle','-')
+        
+        midTarget = t1(nbTarget) + (durTarget/2);
+        text(midTarget, yValMax+(labWinHight/2)+2, labTarget{nbTarget});
+
+        % draw feature track
+        ta = t1(nbTarget);
+        tb = ta + obj.durTransition(nbTarget);
+        tc = tb + obj.durHold(nbTarget);
+        
+        taVal = preceedingVal;
+        tbVal = featureVals(nbTarget);
+        tcVal = tbVal;
+        
+        %tLab2Val = yValsTmp(nbTarget)
+        
+        plot([ta tb], [taVal tbVal], plotStr);
+        plot([tb tc], [tbVal tcVal], plotStr);
+        
+        preceedingVal = tcVal;
+        
+    end
+    
+   
+    axis([0 t2(end) yValMin-2 yValMax+labWinHight+2])
+    grid on
+    xlabel('time [sec]')
+    ylabel([featureString ' [\Delta\lambda]'])
 end
 
