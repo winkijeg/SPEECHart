f585eb95303534c09b5a144480420edc90a8586c
winkijeg <winkler.phonetics@googlemail.com>
2015-07-27 12:19:39 +0200

    add documentation

--- code_modelling/@UtterancePlan/UtterancePlan.m
+++ code_modelling/@UtterancePlan/UtterancePlan.m
@@ -1,30 +1,30 @@
 classdef UtterancePlan
-    % keep utterance specification necessary for tongue movement simulation
-    %   In order to simulate tonge movement three kind of data have to be 
-    %   specified:
-    %
-    % delta lambda commands in mm for all phonemes of the sequence except
-    % the initial rest position - These values are referenced to the value
-    % at rest (negativ values == activation)
-    %
-    % For the jaw, the lips and the larynx the time variations are made
-    % according to an undamped second order model (Bell-shaped velocity profile)
+    % keep utterance specification necessary for simulate tongue movement
+        %For tonge movement simulation timing and muscle activation have 
+        % to be specified:
+        %
+        % delta lambda commands in mm for all phonemes of the sequence except
+        % the initial rest position - These values are referenced to the value
+        % at rest (negativ values == activation)
+        %
+        % For the jaw, the lips and the larynx the time variations are made
+        % according to an undamped second order model (Bell-shaped velocity profile)
 
     
     properties
         
-        target@char             % target (phonemes) after following initial tongue rest position
+        target@char             % speech-like targets following initial tongue rest position
         
-        durTransition@double    % duration between phonemes (incl. initial rest position)
-        durHold@double          % duration for all phonemes (excdept initial rest position)
+        durTransition@double    % duration between targets (incl. initial rest position)
+        durHold@double          % duration of each target (excdept initial rest position)
 
-        deltaLambdaGGP@double   % delta lambda value for genioglossus posterior [mm ????]
-        deltaLambdaGGA@double   % delta lambda value for genioglossus anterior [mm ????]
-        deltaLambdaHYO@double   % delta lambda value for hyoglossus [mm ????]
-        deltaLambdaSTY@double   % delta lambda value for styloid [mm ????]
-        deltaLambdaVER@double   % delta lambda value for verticales [mm ????]
-        deltaLambdaSL@double    % delta lambda value for superior longitudinalis [mm ????]
-        deltaLambdaIL@double    % delta lambda value for inferior longitudinalis [mm ????]
+        deltaLambdaGGP@double   % delta lambda value for genioglossus posterior
+        deltaLambdaGGA@double   % delta lambda value for genioglossus anterior
+        deltaLambdaHYO@double   % delta lambda value for hyoglossus
+        deltaLambdaSTY@double   % delta lambda value for styloid
+        deltaLambdaVER@double   % delta lambda value for verticales
+        deltaLambdaSL@double    % delta lambda value for superior longitudinalis
+        deltaLambdaIL@double    % delta lambda value for inferior longitudinalis
 
         jawRotation@double      % successive jaw rotation angles [degrees]; positiv == opening
         % These commands are not made in reference to the position at rest but to the position for the preceding phoneme
@@ -43,10 +43,10 @@ classdef UtterancePlan
     methods
         
         function obj = UtterancePlan(targets)
-            % Take a phoneme string as single argument, i.e. UtterancePlan('ai')
+            % Take target string as single argument, i.e. UtterancePlan('ai')
             %   the constructor then produces an utterance specification by 
             %   itializing the properties to standard values
-            %   - duration == 150 ms for each target
+            %   - duration == 150 ms for each speech-like target
             %   - deltaLambda == 0 for each muscle at each target
             %   - no movement of the remaining articulators
             %
